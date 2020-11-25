1e0d5d0d9671e5254ccbd93e73f775bec9c5816f
Саня <sanya@Sergejs-MacBook-Air.local>
2020-11-25 16:36:25 +0600

    Первый проект на Unity

---
+++ Assets/Scripts/CameraScr.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScr : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+
+    void Start()
+    {
+        offset = new Vector3(0, 1.5f, -4);
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    public int speed = 20;
+    public int speedRight = 15;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        rb.AddForce(0, 0, speed);
+
+        if (Input.GetKey(KeyCode.A))
+            rb.AddForce(-speedRight, 0, 0);
+            
+        if (Input.GetKey(KeyCode.D))
+            rb.AddForce(speedRight, 0, 0);
+
+        if (transform.position.y < -5)
+            Application.LoadLevel(Application.loadedLevel);
+    }
+}
